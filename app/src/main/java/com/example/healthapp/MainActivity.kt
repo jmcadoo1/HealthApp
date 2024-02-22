77b9bbbbe58604f1d53b5c606a5a0bec9f0edde6
mgammi <111652473+mgammi@users.noreply.github.com>
2024-02-22 12:10:47 -0500

    added stuff to layout_text

--- app/src/main/java/com/example/healthapp/MainActivity.kt
+++ app/src/main/java/com/example/healthapp/MainActivity.kt
@@ -3,12 +3,29 @@ package com.example.healthapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.TextClock
+
 
 
 class MainActivity : AppCompatActivity() {
+    // on below line we are creating a variable.
+    lateinit var app: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout_text)
+
+//        // on below line we are initializing our variables.
+//        app = findViewById(R.id.text)
+//
+//        // on below line we are setting message for our text view.
+//        app.text = "Welcome to Geeks for Geeks"
+
+       // setMovementMethod(new ScrollingMovementMethod());
+
 
     }
+
 }
+
