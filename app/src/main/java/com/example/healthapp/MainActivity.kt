58493d99a7d0b75ffc34533a419f73985ee97694
mgammi <111652473+mgammi@users.noreply.github.com>
2024-02-22 12:16:29 -0500

    didnt do anything

--- app/src/main/java/com/example/healthapp/MainActivity.kt
+++ app/src/main/java/com/example/healthapp/MainActivity.kt
@@ -9,20 +9,9 @@ import android.widget.TextClock
 
 
 class MainActivity : AppCompatActivity() {
-    // on below line we are creating a variable.
-    lateinit var app: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_text)
-
-//        // on below line we are initializing our variables.
-//        app = findViewById(R.id.text)
-//
-//        // on below line we are setting message for our text view.
-//        app.text = "Welcome to Geeks for Geeks"
-
-       // setMovementMethod(new ScrollingMovementMethod());
+        setContentView(R.layout.activity_main)
 
 
     }
