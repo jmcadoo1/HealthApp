d547a9b90a415a639b18ae0ec3f5fd00f0c7de2f
John A McAdoo <john.a.mcadoo@gmail.com>
2024-02-22 12:11:14 -0500

    dropped the website where we were inspired from... also made a class for the ChoicesFragmentas well!

--- app/src/main/java/com/example/healthapp/ChoicesFragment.kt
+++ app/src/main/java/com/example/healthapp/ChoicesFragment.kt
@@ -6,15 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class TextFragment: Fragment(){
+class ChoicesFragment: Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(
-            R.layout.layout_text, container, false
+            R.layout.layout_choices, container, false
         )
     }
-    // Here "layout_text" is a name of layout file
-    // created for TextFragment
+    // Here "layout_choices" is a name of layout file
+    // created for ChoicesFragment
     //inspired from https://www.geeksforgeeks.org/how-to-implement-tabs-viewpager-and-fragment-in-android-using-kotlin/
 }
