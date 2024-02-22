3de834c0f564201ea2bb9d66e9cc964b6fb80fd2
John A McAdoo <john.a.mcadoo@gmail.com>
2024-02-22 12:54:43 -0500

    just copied over the stuff and changed to what we have created... not certain if it works though because my emulator is being goofy

--- app/src/main/java/com/example/healthapp/MainActivity.kt
+++ app/src/main/java/com/example/healthapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.healthapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +15,63 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var tab_viewpager = findViewById<ViewPager>(R.id.viewPager)
+        var tab_tablayout = findViewById<TabLayout>(R.id.tabbyCat)
+
+        // As we set NoActionBar as theme to this activity
+        // so when we run this project then this activity doesn't
+        // show title. And for this reason, we need to run
+        // setSupportActionBar method
+        setupViewPager(tab_viewpager)
+
+        // If we dont use setupWithViewPager() method then
+        // tabs are not used or shown when activity opened
+        tab_tablayout.setupWithViewPager(tab_viewpager)
     }
+
+    private fun setupViewPager(viewpager: ViewPager) {
+        var adapter: ViewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        // TextFragment is the name of Fragment and the text
+        // is a title of tab
+        adapter.addFragment(TextFragment(), "Text")
+        adapter.addFragment(ChoicesFragment(), "Choices")
+
+        // setting adapter to view pager.
+        viewpager.setAdapter(adapter)
+    }
+
+    class ViewPagerAdapter : FragmentPagerAdapter {
+
+        // objects of arraylist. One is of Fragment type and
+        // another one is of String type.*/
+        private final var fragmentList1: ArrayList<Fragment> = ArrayList()
+        private final var fragmentTitleList1: ArrayList<String> = ArrayList()
+
+        // this is a secondary constructor of ViewPagerAdapter class.
+        public constructor(supportFragmentManager: FragmentManager)
+                : super(supportFragmentManager)
+
+        // returns which item is selected from arraylist of fragments.
+        override fun getItem(position: Int): Fragment {
+            return fragmentList1.get(position)
+        }
+
+        // returns which item is selected from arraylist of titles
+        override fun getPageTitle(position: Int): CharSequence {
+            return fragmentTitleList1.get(position)
+        }
+
+        // returns the number of items present in arraylist.
+        override fun getCount(): Int {
+            return fragmentList1.size
+        }
+
+        // this function adds the fragment and title in 2 separate  arraylist.
+        fun addFragment(fragment: Fragment, title: String) {
+            fragmentList1.add(fragment)
+            fragmentTitleList1.add(title)
+        }
+    }
+
 }
