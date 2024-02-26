c1e6ec6b284be8043952710676a6ce0c0d61cf96
mgammi <111652473+mgammi@users.noreply.github.com>
2024-02-25 21:31:13 -0500

    added fragments for tabs 5 and 6

--- app/src/main/java/com/example/healthapp/LowerRightFragment.kt
+++ app/src/main/java/com/example/healthapp/LowerRightFragment.kt
@@ -1,4 +1,17 @@
 package com.example.healthapp
 
-class LowerRightFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LowerRightFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.layout_lowerright, container, false
+        )
+    }
 }
