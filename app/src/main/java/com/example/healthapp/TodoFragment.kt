1751b1816c230c3a17c8a65fe9d0b59392f0deb0
viyer12 <viyer12@terpmail.umd.edu>
2024-04-10 06:13:10 -0400

    adding vertical functionality to exercise tab

--- app/src/main/java/com/example/healthapp/TodoFragment.kt
+++ app/src/main/java/com/example/healthapp/TodoFragment.kt
@@ -1,14 +1,20 @@
 package com.example.healthapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
-import android.widget.Button
+import android.widget.SearchView
+import android.widget.Spinner
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.widget.ArrayAdapter
 
 
 class TodoFragment : Fragment() {
@@ -24,41 +30,62 @@ class TodoFragment : Fragment() {
 
     ): View? {
 
-        /*return inflater.inflate(
-            R.layout.layout_todo, container, false
-        )*/
         val rootView = inflater.inflate(R.layout.layout_todo, container, false)
-        editTextNewItem = rootView.findViewById(R.id.editText)
-        listViewItems = rootView.findViewById(R.id.listView)
-        itemsAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, todoItems)
-        listViewItems.adapter = itemsAdapter
-        rootView.findViewById<Button>(R.id.add).setOnClickListener {
-            addNewItem()
-        }
-        listViewItems.setOnItemClickListener { _, _, position, _ ->
-            val currentItem = todoItems[position]
-            if(!currentItem.endsWith(" ✔")) {
-                todoItems[position] = todoItems[position] + " ✔"
-            } else {
-                todoItems[position] = currentItem.removeSuffix(" ✔")
+
+        var counter = 1
+
+        var mintotal = 0
+
+        var caltotal = 0
+
+        val search: SearchView = rootView.findViewById(R.id.editSearch)
+
+        val spinner: Spinner = rootView.findViewById(R.id.spinner)
+
+        val calories: EditText = rootView.findViewById(R.id.calsBurned)
+
+        val button: Button = rootView.findViewById(R.id.add)
+
+        val table: TableLayout = rootView.findViewById(R.id.table)
+
+        val totals: TableLayout = rootView.findViewById(R.id.totals)
+
+        button.setOnClickListener {
+            val act = TextView(container!!.context)
+            act.text = search.query.toString()
+            act.textSize = 24F
+            val tim = TextView(container.context)
+            tim.text = spinner.selectedItem.toString()
+            tim.textSize = 24F
+            val cal = TextView(container.context)
+            cal.text = calories.text.toString()
+            cal.textSize = 24F
+
+            if (cal.text == "") {
+                cal.text = "" + (tim.text.toString().toInt() * 5)
             }
-            itemsAdapter.notifyDataSetChanged()
+
+            mintotal += tim.text.toString().toInt()
+            caltotal += cal.text.toString().toInt()
+
+            addNewItem(container.context, table, counter, act, tim, cal)
+            counter += 1
+            val row = totals.getChildAt(0) as TableRow
+            var item = row.getChildAt(1) as TextView
+            item.text = "" + mintotal
+            item = row.getChildAt(2) as TextView
+            item.text = "" + caltotal
         }
-        listViewItems.setOnItemLongClickListener { _, _, position, _ ->
-            todoItems.removeAt(position)
-            itemsAdapter.notifyDataSetChanged()
-            true
-        }
+
         return rootView
     }
 
-    private fun addNewItem() {
-        val newItem = editTextNewItem.text.toString()
-        if (newItem.isNotBlank()) {
-            todoItems.add(newItem)
-            itemsAdapter.notifyDataSetChanged()
-            editTextNewItem.text.clear()
-        }
+    private fun addNewItem(context: Context, table: TableLayout, counter: Int, activity: TextView, time: TextView, cals: TextView) {
+        table.addView(TableRow(context))
+        val row = table.getChildAt(counter) as TableRow
+        row.addView(activity)
+        row.addView(time)
+        row.addView(cals)
     }
     // Here "layout_todo" is a name of layout file
     // created for TodoFragment
